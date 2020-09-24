cbc31cc0dddd0dbf8a6fa3543661ee48b88ab470
Bryan Reese <bryanreese@fb.com>
2020-09-24 09:11:13 -0700

    Added as submodule instead of doing a clone

--- astra-sim/build.sh
+++ astra-sim/build.sh
@@ -1,41 +1,34 @@
 #! /bin/bash
 
 # Download network backend
-cd network
-if [ -d "gem5_astra" ]
+if [ -d "network/gem5_astra" ]
 then
     echo "You have already downloaded gem5"
-    cd ..
-elif [ -d "ns3" ]
+elif [ -d "network/ns3" ]
 then
     echo "You have already downloaded ns3"
-    cd ..
 else
     read -p "Enter the back-end you want to download, options are gem5 or ns3: " backend
 
     if [ "$backend" == "gem5" ]
     then
-        git clone https://github.com/georgia-tech-synergy-lab/gem5_astra.git
-        echo "gem5 has been successfully downloaded"
+        git submodule add https://github.com/georgia-tech-synergy-lab/gem5_astra.git network/gem5_astra && \
+            echo "gem5 has been successfully downloaded" || \
+            echo "gem5 downloaded did not succeed!"
     elif [ "$backend" == "ns3" ]
     then
-        git clone
-        echo "ns3 has been successfully downloaded"
+        git submodule add https://github.com/nsnam/ns-3-dev-git.git network/ns3 && \
+            echo "ns3 has been successfully downloaded" || \
+            echo "ns3 downloaded did not succeed!"
     fi
-    cd ..
 fi
 
 # Download compute backend
-cd compute
-if [ -d "SCALE-Sim" ]
+if [ -d "compute/SCALE-Sim" ]
 then
     echo "You have already downloaded SCALE-Sim"
-    cd ..
-    exit 1
+else
+    git submodule add https://github.com/ARM-software/SCALE-Sim.git compute/SCALE-Sim && \
+    echo "SCALE-Sim has been successfully downloaded" || \
+    echo "SCALE-Sim downloaded did not succeed!"
 fi
-
-git clone https://github.com/ARM-software/SCALE-Sim.git
-echo "SCALE-Sim has been successfully downloaded"
-cd ..
-
-exit 1
